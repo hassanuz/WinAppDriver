165470032aa45ad7b4a9b3c0c0c35c086dc28195
Hassan Uraizee <hauraize@microsoft.com>
2019-09-23 22:57:39 -0700

    Update CalculatorSession.cs

--- Samples/C#/CalculatorTest/CalculatorSession.cs
+++ Samples/C#/CalculatorTest/CalculatorSession.cs
@@ -39,7 +39,7 @@ namespace CalculatorTest
                 DesiredCapabilities appCapabilities = new DesiredCapabilities();
                 appCapabilities.SetCapability("app", CalculatorAppId);
                 appCapabilities.SetCapability("deviceName", "WindowsPC");
-                appCapabilities.SetCapability("ms:waitForAppLaunch", "25");
+                appCapabilities.SetCapability("ms:waitForAppLaunch", "35");
                 session = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), appCapabilities);
                 Assert.IsNotNull(session);
 
